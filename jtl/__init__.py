8748160a2e94ff4a5d6cc5f03f866ff3f8e1eccd
Ian Hutchinson <ihutchinson@agalmicventures.com>
2015-04-03 21:43:51 -0500

    Stubbed out basic program structure and added some simple test data

--- jtl/__init__.py
+++ jtl/__init__.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
+import json
 import sys
 
-def transform(input, transform):
-	return None #TODO
+def transform(data, transform):
+	#TODO
+	return data
+
+	return None
+
+def transformJson(data, transformData):
+	result = {}
+	for k, v in transformData.items():
+		result[k] = transform(data, v)
+	return result
 
 def main():
 	#TODO: parse arguments
 
-	#TODO: transform
+	data = json.loads(sys.stdin.read())
 
-	#TODO: output
+	transformData = {} #TODO
+
+	#Transform the JSON
+	result = transformJson(data, transformData)
+
+	#Output the result
+	print(json.dumps(data, indent=4, sort_keys=True))
+
 	return 0
 
 if __name__ == '__main__':
